cc05496e793e593512c662c3c26fe68fe1cd08bc
JuanjoGrak <110053275+JuanjoGrak@users.noreply.github.com>
2022-07-27 19:35:05 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>My Favorite Foods</h1>
+    <ul>
+      <li>Randam</li>
+      <li>Beans and Eggs</li>
+      <li>Chicken at Dixy</li>
+      <li>Colombian Hotdogs</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
